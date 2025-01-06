d8c80091bbdafa5c9dc001940f4e7df43f6060bf
idorom <35151777+idorom@users.noreply.github.com>
2025-01-06 19:32:45 +0200

    Update Firebase.js

--- TraStory-main/src/Firebase.js
+++ TraStory-main/src/Firebase.js
@@ -5,13 +5,13 @@ import "firebase/storage"
 
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAJ3dkpFhfBo5BJwjycdDZ9kpGyf4DzgcA",
-  authDomain: "tracetory-4340e.firebaseapp.com",
-  projectId: "tracetory-4340e",
-  storageBucket: "tracetory-4340e.appspot.com",
-  messagingSenderId: "628717194814",
-  appId: "1:628717194814:web:016c6a3be539b88dc44b6f",
-  measurementId: "G-09YT4YQKFK"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 })
 
 // firebase.initializeApp(firebaseConfig)
